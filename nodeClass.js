ade5c3399a9154f414cbcc1d53e5afa72fd589fe
Louis <louis.dubois@epitech.eu>
2026-07-24 21:34:40 +0200

    ADD: Node class

---
+++ nodeClass.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.top = null;
+        this.right = null;
+        this.bottom = null;
+        this.left = null;
+    }
+}
+
+export { Node };
