39be1b5eec17dd02c1d9f19d024f00cea2173340
Louis <louis.dubois@epitech.eu>
2026-07-27 21:33:34 +0200

    FIX: test

--- script.test.js
+++ script.test.js
@@ -96,7 +96,7 @@ test.each(allExpected)("Node [$x][$y] has correct knight moves", ({ x, y, moves 
 });
 
 test("Path from [0, 0] to [3, 3]", () => {
-    expect(knightMoves([0, 0], [3, 3])).toEqual([[0,0],[2, 1],[3,3]]);
+    expect(knightMoves([0, 0], [3, 3])).toEqual([[0,0],[2,1],[3,3]]);
 });
 
 test("Path from [3, 3] to [0, 0]", () => {
@@ -104,5 +104,5 @@ test("Path from [3, 3] to [0, 0]", () => {
 });
 
 test("Path from [0, 0] to [7, 7]", () => {
-    expect(knightMoves([0, 0], [7, 7])).toEqual([[0,0],[1, 2],[3, 3],[5, 4],[7,5],[5,6],[7,7]]);
+    expect(knightMoves([0, 0], [7, 7])).toEqual([[0,0],[2,1],[4,2],[6,3],[7,5],[5,6],[7,7]]);
 });
